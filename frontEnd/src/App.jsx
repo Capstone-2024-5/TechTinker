9ea81e9443877908aa45cdda17e9f25f547a448a
Susmi Vijayan <susmivijayan51@gmail.com>
2024-02-02 14:06:37 -0500

    Added roting for Course Registration Page

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-import '../public/styles.css'; 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Update import
+import '../public/styles.css';
 import Header from './components/shared/Header.jsx';
 import Footer from './components/shared/Footer.jsx';
+import CourseRegistration from './components/CourseRegistration.jsx';
 
 class App extends React.Component {
     render() {
         return (
-            <>
-                <Header />
-                <Footer />
-            </>
+            <Router>
+                <>
+                    <Header />
+                    <Routes>  
+                        <Route path="/register" element={<CourseRegistration />} />
+                       
+                    </Routes>
+                    <Footer />
+                </>
+            </Router>
         );
     }
 }
