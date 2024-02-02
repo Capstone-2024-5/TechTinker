d4b8ca55918cfde198a827390dbef0dbc526b05a
bhuvaneshwari-sivasangaran <bhuvaneshwari.sivasangaran@gmail.com>
2024-02-02 18:26:22 -0500

    Committing Home Page changes

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import '../public/styles.css'; 
-import Header from './components/shared/Header.jsx';
+import Header from './components/shared/header.jsx';
 import Footer from './components/shared/Footer.jsx';
+import { Home } from "@mui/icons-material";
 
 class App extends React.Component {
     render() {
         return (
             <>
                 <Header />
+                <Home />
                 <Footer />
             </>
         );
---
+++ frontEnd/src/components/Home.jsx
@@ -0,0 +1,145 @@
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { Typography } from "@mui/material";
+
+export default function Home() {
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '"></span>';
+        },
+    };
+
+    return (
+        <>
+          <div className="bgColorPrimary">
+          <Swiper
+                pagination={pagination}
+                modules={[Pagination, Autoplay, Navigation]}
+                className="mySwiper"
+                navigation={true}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+            >
+                <SwiperSlide>
+                    <Grid container sx={{ px: 12 }}>
+                        <Grid item xs={12} md={8} sx={{ px: 6 }}>
+                            <Typography
+                                sx={{ pt: 10 }}
+                                variant="h2"
+                                className="fontMontserrat fontWeight-1000 textSecondary"
+                            >
+                                Ignite your STEM Passion with Fun Learning!
+                            </Typography>
+                            <Typography className="textBlack" variant="h5" sx={{ pt: 6, pb: 12 }}>
+                                Join our dynamic STEM classes, weekdays or
+                                weekends, for hands-on coding and robotics fun.
+                                Unleash curiosity, embrace innovation – enroll
+                                now!
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={0} md={4}></Grid>
+                    </Grid>
+                </SwiperSlide>
+                <SwiperSlide>
+                <Grid container sx={{ px: 12 }}>
+                        <Grid item xs={12} md={8} sx={{ px: 6 }}>
+                            <Typography
+                                sx={{ pt: 10 }}
+                                variant="h2"
+                                className="fontMontserrat fontWeight-1000 textSecondary"
+                            >
+                                Ignite your STEM Passion with Fun Learning!
+                            </Typography>
+                            <Typography className="textBlack" variant="h5" sx={{ pt: 6, pb: 12 }}>
+                                Join our dynamic STEM classes, weekdays or
+                                weekends, for hands-on coding and robotics fun.
+                                Unleash curiosity, embrace innovation – enroll
+                                now!
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={0} md={4}></Grid>
+                    </Grid>
+                </SwiperSlide>
+            </Swiper>
+            <Typography
+                variant="h3"
+                className="textCenter fontWeight-800"
+                sx={{ py: 1, mt: 8, mb: 2 }}
+            >
+                Why choose us for STEM Education?
+            </Typography>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container padding={5}>
+                    <Grid item xs={12} sm={12} md={6} sx={{ px: 6, pb: 6 }}>
+                        <Box className="textCenter">
+                            <Typography variant="h5" sx={{ pb: 2 }}>
+                                Fostering Mechanical Minds
+                            </Typography>
+                            <p>
+                                At our robotics program, we ignite the spark for
+                                mechanical thinking. Whether you're drawn to the
+                                intricacies of machinery or fascinated by
+                                building, our curriculum caters to diverse
+                                interests within the STEM spectrum.
+                            </p>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={6} sx={{ px: 6, pb: 6 }}>
+                        <Box className="textCenter">
+                            <Typography variant="h5" sx={{ pb: 2 }}>
+                                Collaborative Learning Environment
+                            </Typography>
+                            <p>
+                                Step into a collaborative arena where teamwork
+                                is not just encouraged but essential. Our small
+                                class sizes ensure an intimate setting, with a
+                                maximum of 8 students per instructor,
+                                facilitating effective collaboration and
+                                personalized attention.
+                            </p>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={6} sx={{ px: 6, pb: 6 }}>
+                        <Box className="textCenter">
+                            <Typography variant="h5" sx={{ pb: 2 }}>
+                                Experiential Exploration
+                            </Typography>
+                            <p>
+                                Experience STEM education through hands-on
+                                projects and experiments. Our curriculum is
+                                designed to engage young minds in real-world
+                                problem-solving, fostering a deep understanding
+                                of concepts through practical application.
+                            </p>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={6} sx={{ px: 6, pb: 6 }}>
+                        <Box className="textCenter" sx={{ pb: 2 }}>
+                            <Typography variant="h5">
+                                Passion-Driven Instructors
+                            </Typography>
+                            <p>
+                                Learn from a dedicated team of instructors who
+                                are not just educators but passionate
+                                enthusiasts of robotics and engineering. Our
+                                staff is committed to instilling a love for
+                                learning, making the educational journey both
+                                exciting and impactful.
+                            </p>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Box>
+          </div>
+        </>
+    );
+}
