cc322daa51473b75e5a985455ecb61036b378f52
Susmi Vijayan <susmivijayan51@gmail.com>
2024-02-12 15:21:50 -0500

    Course_Registration API

--- backEnd/server.js
+++ backEnd/server.js
@@ -8,8 +8,15 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 const userRouter = require("./routes/users_route");
+const registrationRouter = require("./routes/registration_route");
+const { registerUser } = require("./controllers/registration_controller");
+
 app.use("/user", userRouter);
+app.use("/api", registrationRouter);
+
+// Use registerUser as the route handler for POST /api/register
+app.post("/api/register", registerUser);
 
 const PORT = process.env.API_SERVER_PORT || 5000;
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.listen(PORT, console.log(`Server API running on port ${PORT}`));
