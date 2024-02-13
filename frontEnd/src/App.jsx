584accb051128dacdae64d102416ceca6d947465
bhuvaneshwari-sivasangaran <bhuvaneshwari.sivasangaran@gmail.com>
2024-02-12 19:09:34 -0500

    committing sprint 1 changes

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -6,6 +6,7 @@ import Header from "./components/shared/Header.jsx";
 import Footer from "./components/shared/Footer.jsx";
 import Home from "./components/Home.jsx";
 import CourseRegistration from "./components/CourseRegistration.jsx";
+import Contact from "./components/Contact.jsx";
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/course" element={<CourseRegistration />} />
+                <Route path="/contact-us" element={<Contact />} />
             </Routes>
             <Footer />
         </Router>
